d5a656120a5d399fbe263763b5c598cf8e9d794f
RakibulRanak <43205114+RakibulRanak@users.noreply.github.com>
2019-11-18 22:51:08 +0600

    Update CoinChangeWithMinimumCoin.cpp

--- DP/CoinChange/CoinChangeWithMinimumCoin/CoinChangeWithMinimumCoin.cpp
+++ DP/CoinChange/CoinChangeWithMinimumCoin/CoinChangeWithMinimumCoin.cpp
@@ -1,38 +1,39 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int minCoins(int coins[], int m, int V)
 {
+    // m is size of coins array (number of different coins)
+
+    // table[i] will be storing the minimum number of coins
+    // required for i value. So table[V] will have result
+ 
     int table[V + 1];
 
-    // Base case (If given value V is 0)
+    //base case
     table[0] = 0;
-
-    // Initialize all table values as Infinite
+    // Initialize all table values as a higher value
     for (int i = 1; i <= V; i++)
-        table[i] = 100;
+        table[i] = 1000000;
 
     // Compute minimum coins required for all
     // values from 1 to V
 
-    for (int i = 1; i <= V; i++)
-    {
+    for (int i = 0; i < m; i++)
+    {   
         // Go through all coins smaller than i
-        for (int j = 0; j < m; j++)
-
-            if (coins[j] <= i)
-            {
-                int sub_res = table[i - coins[j]];
-                if (sub_res != INT_MAX && sub_res + 1 < table[i])
-                    table[i] = sub_res + 1;
-            }
+        for (int j = coins[i]; j <= V; j++)
+            table[j]=min(table[j-coins[i]]+1 ,table[j]);
     }
     return table[V];
 }
 
+
 int main()
 {
-    int coins[] = {9, 6, 5};
+    int coins[] = {1, 5, 6,8};
     int m = sizeof(coins) / sizeof(coins[0]);
     int V = 11;
     cout << "Minimum coins required is "
