bb6e46b9f1a600106ca417f05a00c85e65b4c70a
Akshay Khatter <30326830+akshaykhatter@users.noreply.github.com>
2022-10-08 02:49:44 +0530

    added insertion sort in cpp and python

---
+++ Sorting/Insertion sort/insertionSort.py
@@ -0,0 +1,23 @@
+# creating a function for insertion  
+def insertion_sort(list1):  
+  
+        # Outer loop to traverse through 1 to len(list1)  
+        for i in range(1, len(list1)):  
+  
+            value = list1[i]  
+  
+            # Move elements of list1[0..i-1], that are  
+            # greater than value, to one position ahead  
+            # of their current position  
+            j = i - 1  
+            while j >= 0 and value < list1[j]:  
+                list1[j + 1] = list1[j]  
+                j -= 1  
+            list1[j + 1] = value  
+        return list1  
+            # Driver code to test above  
+  
+list1 = [10, 5, 13, 8, 2]  
+print("The unsorted list is:", list1)  
+  
+print("The sorted list1 is:", insertion_sort(list1))  
