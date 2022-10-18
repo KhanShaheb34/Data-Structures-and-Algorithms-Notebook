b4ca480eb7ec14f47167171a503ff223abf2efb7
Tanya Verma <vermatanya932@gmail.com>
2022-10-18 23:14:38 +0530

    Flatten_LinkedList

---
+++ BasicDataStructures/LinkedList/FlattenList.cpp
@@ -0,0 +1,154 @@
+
+#include <bits/stdc++.h>
+
+// Linked List Node Structure
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+
+Node* mergeList(Node* headA,Node* headB)
+{
+    Node* head =NULL;
+    Node* tail =NULL;
+    while(headA && headB){
+    if(head==NULL)
+    {
+        if(headA->data < headB->data)
+        {
+            head = headA;
+            tail = headA;
+            headA = headA->bottom;
+        }
+        else{
+            head = headB;
+            tail = headB;
+            headB = headB->bottom;
+        }
+    }
+    else{
+        if(headA->data < headB->data)
+        {
+            tail->bottom = headA;
+            tail = headA;
+            headA= headA->bottom;
+            
+        }
+        else
+        {
+            tail->bottom = headB;
+            tail = headB;
+            headB= headB->bottom;
+        }
+    }
+    if(headA)
+    {
+        tail->bottom = headA;
+        
+    }
+    if(headB)
+    {
+        tail->bottom = headB;
+       
+    }
+    }
+    return head;
+}
+
+Node *flatten(Node *root)
+{
+    if(root==NULL)
+    return NULL;
+    if(root->next ==NULL)
+    return root;
+    Node *curr = root; 
+   return mergeList(root,flatten(root->next));
+  
+    }
