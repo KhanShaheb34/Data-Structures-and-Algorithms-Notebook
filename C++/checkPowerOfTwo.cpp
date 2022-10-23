a2544e04a37586a0844c4b7a3f1c5c49fe92c6ad
Devesh Kumar <97828805+deveshpandee@users.noreply.github.com>
2022-10-23 20:34:03 +0530

    Create checkPowerOfTwo.cpp

---
+++ C++/checkPowerOfTwo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if(a&a-1)
+    cout<<a<<" is not power of 2"<<endl;
+    else
+    cout<<a<<" is power of 2"<<endl;
+
+}
