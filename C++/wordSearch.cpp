0977251e902bfd7b70bca377b6e7a78022415d5c
04018002718 <91694473+04018002718@users.noreply.github.com>
2022-10-25 18:30:17 +0530

    Update wordSearch.cpp

--- C++/wordSearch.cpp
+++ C++/wordSearch.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    
     bool solve(vector<vector<char>>& board, int i, int j, int ind, string& word, int n, int m){
         if(ind == word.size()) return true;
         if(i<0 || j<0 || i>=n || j>=m || word[ind]!=board[i][j])
             return false;
-        
         char temp=board[i][j];
         board[i][j]='#';
-        
         bool ans =  solve(board, i+1, j, ind+1, word, n, m) || 
                     solve(board, i, j+1, ind+1, word, n, m) ||
                     solve(board, i-1, j, ind+1, word, n, m) ||
                     solve(board, i, j-1, ind+1, word, n, m);
-        
-        board[i][j]=temp;
+                board[i][j]=temp;
         return ans;
     }
-    
     bool exist(vector<vector<char>>& board, string word) {
         int n=board.size(), m=board[0].size();
-        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(board[i][j]==word[0]){
