cb9971bbc8be6f65d369455f06bc75b93daa4d91
Rahul <50038276+itsrahulhere@users.noreply.github.com>
2022-10-25 18:15:43 +0530

    Update and rename Recover Binary Search Tree.cpp to RecoverBinarySearchTree.cpp

--- C++/RecoverBinarySearchTree.cpp
+++ C++/RecoverBinarySearchTree.cpp
@@ -1,15 +1,9 @@
 class Solution {
-
     TreeNode* first, *middle, *prev, *last;
-    
     void inorder(TreeNode* root)
-    {
-        if(!root) return ;
-        
-        inorder(root->left);
-        
-        if(prev && (prev->val > root->val))
-        {
+    {   if(!root) return ;
+        inorder(root->left)
+        if(prev && (prev->val > root->val)) {
             if(first==NULL){
                 first=prev;
                 middle=root;
@@ -18,11 +12,8 @@ class Solution {
                 last=root;
         }
         prev=root;
-        
         inorder(root->right);
-        
-    }
-    
+    } 
 public:
     void recoverTree(TreeNode* root) {
         first=middle=last=NULL;
