f055c0b316ba02565ba0576fa208b721c2cd5d2f
Rahul <50038276+itsrahulhere@users.noreply.github.com>
2022-10-25 18:10:48 +0530

    Create Recover Binary Search Tree.cpp

---
+++ C++/Recover Binary Search Tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+
+    TreeNode* first, *middle, *prev, *last;
+    
+    void inorder(TreeNode* root)
+    {
+        if(!root) return ;
+        
+        inorder(root->left);
+        
+        if(prev && (prev->val > root->val))
+        {
+            if(first==NULL){
+                first=prev;
+                middle=root;
+            }
+            else
+                last=root;
+        }
+        prev=root;
+        
+        inorder(root->right);
+        
+    }
+    
+public:
+    void recoverTree(TreeNode* root) {
+        first=middle=last=NULL;
+        prev=new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last) swap(first->val, last->val);
+        else if(first && middle) swap(first->val, middle->val);
+    }
+};
