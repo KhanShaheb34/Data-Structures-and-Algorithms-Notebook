00a732182ea3fd70898edccf0482e9cd6cb97033
04018002718 <91694473+04018002718@users.noreply.github.com>
2022-10-25 18:27:23 +0530

    Update and rename Subsets.cpp to subsets.cpp

--- C++/subsets.cpp
+++ C++/subsets.cpp
@@ -1,29 +1,17 @@
-vector<vector < int>> subsets(vector<int> &nums)
-        {
+vector<vector < int>> subsets(vector<int> &nums){
             int n = nums.size();
-
             vector<vector < int>> ans;
-
             for (int i = 0; i < (1 << n); i++)
-
             {
-
                 vector<int> temp;
-
                 for (int j = 0; j < n; j++)
-
                 {
-
                     if (i &(1 << j))
-
                     {
-
                         temp.push_back(nums[j]);
-                    }
-                }
-
+                   }
+               }
                 ans.push_back(temp);
-            }
-
+           }
             return ans;
-        }
+}
