deacb3c682109407d79d8417afd3a1a3d7a53711
Rahul <91694668+dtc040@users.noreply.github.com>
2022-10-26 20:50:34 +0530

    Update rotateList.cpp

--- rotateList.cpp
+++ rotateList.cpp
@@ -1,19 +1,67 @@
-ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head->next || k==0) return head;
-        int n=1;
-        ListNode*end=head,*curr=head;
-        while(end->next){
-            n++;
-            end=end->next;
-        }
-        k=k%n;
-        if(k==0) return head;
-        k=n-k;
-        while(k-- > 1){
-            curr=curr->next;
-        }
-        end->next=head;
-        head=curr->next;
-        curr->next=NULL;
-        return head;
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node* next;
+};
+
+void rotate(Node** head_ref, int k)
+{
+	if (k == 0)
+		return;
+
+	Node* current = *head_ref;
+        
+	int count = 1;
+	while (count < k && current != NULL) {
+		current = current->next;
+		count++;
+	}
+        
+	if (current == NULL)
+		return;
+                
+	Node* kthNode = current;
+
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = *head_ref;
+        *head_ref = kthNode->next;
+	kthNode->next = NULL;
+}
+
+void push(Node** head_ref, int new_data)
+{
+	Node* new_node = new Node();
+	new_node->data = new_data;
+	new_node->next = (*head_ref);
+	(*head_ref) = new_node;
+}
+
+void printList(Node* node)
+{
+	while (node != NULL) {
+		cout << node->data << " ";
+		node = node->next;
+	}
+}
+
+int main(void)
+{
+	Node* head = NULL;
+
+	for (int i = 60; i > 0; i -= 10)
+		push(&head, i);
+
+	cout << "Given linked list \n";
+	printList(head);
+	rotate(&head, 4);
+
+	cout << "\nRotated Linked list \n";
+	printList(head);
+
+	return (0);
 }
