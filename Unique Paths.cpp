458c47d55caef428b3b151f44b9c5400093cf19b
Rahul <91694668+dtc040@users.noreply.github.com>
2022-10-26 20:46:29 +0530

    Update Unique Paths.cpp
    Unique paths in a Grid with Obstacles

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -1,15 +1,35 @@
- int uniquePaths(int m, int n) {
-        vector<int>prev(n);
-        for(int i=0; i<n; i++){
-            prev[i] = 1;
-        }
-        for(int i=1; i<m; i++){
-            vector<int>curr(n);
-            curr[0]=1;
-            for(int j=1; j<n; j++){
-                curr[j] = prev[j] + curr[j-1];
-            }
-            prev=curr;
-        }
-        return prev[n-1];
- }
+#include<bits/stdc++.h>
+using namespace std;
+
+int UniquePathHelper(int i, int j, int r, int c, vector<vector<int>>& A){
+	 if(i == r || j == c){
+	 return 0 ;
+}
+
+	if(A[i][j] == 1){
+	 return 0 ;
+	}
+	
+	if(i == r-1 && j == c-1){
+	 return 1 ;
+	}
+
+	return UniquePathHelper(i+1, j, r, c, A) +
+			UniquePathHelper(i, j+1, r, c, A) ;
+}
+
+
+int uniquePathsWithObstacles(vector<vector<int>>& A)
+{
+	int r = A.size(), c = A[0].size();	
+	return UniquePathHelper(0, 0, r, c, A) ;
+}
+
+int main()
+{
+ vector<vector<int>> A = { { 0, 0, 0 },
+							{ 0, 1, 0 },
+							{ 0, 0, 0 } };
+							
+ cout << uniquePathsWithObstacles(A) << " \n";											
+}
