9c11944e4874ade1bf7f0ffa5c2c291b3084fa16
itsrahulprince <91690860+itsrahulprince@users.noreply.github.com>
2022-10-26 21:30:14 +0530

    Update Trapping Rain Water.cpp

--- C++/Trapping Rain Water.cpp
+++ C++/Trapping Rain Water.cpp
@@ -1,4 +1,7 @@
-  int trap(vector<int>& h) {
+ #include <bits/stdc++.h>
+ using namespace std;
+  
+ int trap(vector<int>& h) {
         int water = 0, l = 0, r = h.size()-1, lmax = INT_MIN, rmax = INT_MIN;
         while(l < r){
             lmax = max(h[l], lmax);
@@ -6,4 +9,11 @@
             water += (lmax < rmax) ? lmax-h[l++] : rmax-h[r--] ;
         }
         return water;
-    }
+  }
+  
+ int main()
+ {
+    vector<int>arr = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+    cout << trap(arr);
+    return 0;
+ }
