7d789b205c88158129d4b0b4b770364e85723c10
04018002718 <91694473+04018002718@users.noreply.github.com>
2022-10-26 21:05:15 +0530

    Update subsets.cpp

--- C++/subsets.cpp
+++ C++/subsets.cpp
@@ -1,17 +1,35 @@
-vector<vector < int>> subsets(vector<int> &nums){
-            int n = nums.size();
-            vector<vector < int>> ans;
-            for (int i = 0; i < (1 << n); i++)
-            {
-                vector<int> temp;
-                for (int j = 0; j < n; j++)
-                {
-                    if (i &(1 << j))
-                    {
-                        temp.push_back(nums[j]);
-                   }
-               }
-                ans.push_back(temp);
-           }
-            return ans;
+#include <bits/stdc++.h>
+using namespace std;
+
+void subsetsUtil(vector<int>& A, vector<vector<int> >& res,	vector<int>& subset, int index)
+{
+	res.push_back(subset);
+	for (int i = index; i < A.size(); i++) {
+            	subset.push_back(A[i]);
+            	subsetsUtil(A, res, subset, i + 1);
+		subset.pop_back();
+	}
+            return;
+}
+
+vector<vector<int> > subsets(vector<int>& A)
+{
+	vector<int> subset;
+	vector<vector<int> > res;
+	int index = 0;
+	subsetsUtil(A, res, subset, index);
+	return res;
+}
+
+int main()
+{
+	vector<int> array = { 1, 2, 3 };
+	vector<vector<int> > res = subsets(array);
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++)
+			cout << res[i][j] << " ";
+		cout << endl;
+	}
+
+	return 0;
 }
