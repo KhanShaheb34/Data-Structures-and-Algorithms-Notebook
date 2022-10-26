1d20cfa519a5bfb390658facda4718040006d8ba
rahulprince546 <91694007+rahulprince546@users.noreply.github.com>
2022-10-26 21:23:20 +0530

    Update N-Queens.cpp

--- C++/N-Queens.cpp
+++ C++/N-Queens.cpp
@@ -1,33 +1,68 @@
-class Solution {
-public:
-    void solve(int col, int n, vector<vector<string>>&ans, vector<string>&board, vector<int>&left, vector<int>&upperD, vector<int>&lowerD){
-        if(col==n){
-            ans.push_back(board);
-            return;
-        }
-        for(int row=0; row<n; row++){
-               if(left[row]==0 && upperD[n-1+col-row]==0 && lowerD[row+col]==0){
-                   left[row]=1;
-                   upperD[n-1+col-row]=1;
-                   lowerD[row+col]=1;
-                   board[row][col]='Q';
-                   solve(col+1, n, ans, board, left, upperD, lowerD);
-                   left[row]=0;
-                   upperD[n-1+col-row]=0;
-                   lowerD[row+col]=0;
-                   board[row][col]='.';
-               }
-        }       
-    }
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string>board(n);
-        string s(n,'.');
-        for(int i=0; i<n; i++){
-            board[i]=s;
-        }
-        vector<int>left(n,0),upperD(2*n-1,0),lowerD(2*n-1,0);
-        solve(0,n,ans,board,left,upperD,lowerD);
-        return ans;
-    }
-};
+#include <bits/stdc++.h>
+#define N 4
+using namespace std;
+
+void printSolution(int board[N][N])
+{
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+			cout << " " << board[i][j] << " ";
+		printf("\n");
+	}
+}
+
+bool isSafe(int board[N][N], int row, int col)
+{
+	int i, j;
+
+	for (i = 0; i < col; i++)
+		if (board[row][i])
+			return false;
+
+	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+		if (board[i][j])
+			return false;
+
+	for (i = row, j = col; j >= 0 && i < N; i++, j--)
+		if (board[i][j])
+			return false;
+
+	return true;
+}
+
+bool solveNQUtil(int board[N][N], int col)
+{
+	if (col >= N)
+		return true;
+	for (int i = 0; i < N; i++) {
+		if (isSafe(board, i, col)) {
+			board[i][col] = 1;
+			if (solveNQUtil(board, col + 1))
+				return true;
+			board[i][col] = 0; // BACKTRACK
+		}
+	}
+	return false;
+}
+
+bool solveNQ()
+{
+	int board[N][N] = { { 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 } };
+
+	if (solveNQUtil(board, 0) == false) {
+		cout << "Solution does not exist";
+		return false;
+	}
+
+	printSolution(board);
+	return true;
+}
+
+int main()
+{
+	solveNQ();
+	return 0;
+}
