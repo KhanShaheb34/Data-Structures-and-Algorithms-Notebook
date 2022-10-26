d42ffba696a775925f3976bf64f4d5e41e4c3961
rahulprince546 <91694007+rahulprince546@users.noreply.github.com>
2022-10-26 21:24:47 +0530

    Update JumpGame.cpp

--- C++/JumpGame.cpp
+++ C++/JumpGame.cpp
@@ -1,8 +1,27 @@
-  bool canJump(vector<int>& nums) {
-        int reach=0;
-        for(int i=0; i<nums.size(); i++){
-            if(reach<i) return false;
-            reach=max(reach, i+nums[i]);
-        }
-        return true;
- } 
+#include <bits/stdc++.h>
+using namespace std;
+
+int minJumps(int arr[], int n)
+{
+	if (n == 1)
+		return 0;
+
+	int res = INT_MAX;
+	for (int i = n - 2; i >= 0; i--) {
+		if (i + arr[i] >= n - 1) {
+			int sub_res = minJumps(arr, i + 1);
+			if (sub_res != INT_MAX)
+				res = min(res, sub_res + 1);
+		}
+	}
+	return res;
+}
+
+int main()
+{
+	int arr[] = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Minimum number of jumps to";
+	cout << " reach the end is " << minJumps(arr, n);
+	return 0;
+}
