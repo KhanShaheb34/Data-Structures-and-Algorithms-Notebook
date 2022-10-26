dc03073eb45007a95c3486e5c6612520df268337
Mohit <71942407+mohit0204@users.noreply.github.com>
2022-10-26 21:15:59 +0530

    Update Search_a_2D_Matrix.cpp

--- C++/Search_a_2D_Matrix.cpp
+++ C++/Search_a_2D_Matrix.cpp
@@ -1,16 +1,30 @@
- bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int m = matrix.size(), n = matrix[0].size();
-    int start = 0, end = m*n - 1;
-    while(start <= end)
-    {
-        int mid = start + (end - start)/2;
-        int e = matrix[mid/n][mid%n];
-        if(target < e)
-            end = mid - 1;    
-        else if(target > e)
-            start = mid + 1;
-        else
-            return true;
-    }
-    return false;
- }
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(int mat[4][4], int n, int x)
+{
+	if (n == 0)
+		return -1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			if (mat[i][j] == x) {
+				cout << "Element found at (" << i << ", "
+					<< j << ")\n";
+				return 1;
+			}
+	}
+
+	cout << "n Element not found";
+	return 0;
+}
+
+int main()
+{
+	int mat[4][4] = { { 10, 20, 30, 40 },
+					{ 15, 25, 35, 45 },
+					{ 27, 29, 37, 48 },
+					{ 32, 33, 39, 50 } };
+	search(mat, 4, 29);
+	return 0;
+}
