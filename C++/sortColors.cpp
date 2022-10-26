a7198bd7bae97433795e92d4f6a1fb2b2e1c1f0a
Mohit <71942407+mohit0204@users.noreply.github.com>
2022-10-26 21:17:37 +0530

    Update sortColors.cpp

--- C++/sortColors.cpp
+++ C++/sortColors.cpp
@@ -1,8 +1,40 @@
-  void sortColors(vector<int>& nums) {
-        int i=0, j=0, k=nums.size()-1;
-        while(j<=k){
-            if(nums[j]==0) swap(nums[i++],nums[j++]);
-            else if(nums[j]==1) j++;
-            else swap(nums[j],nums[k--]);
-        }
-    }
+#include <bits/stdc++.h>
+using namespace std;
+
+void sort012(int a[], int arr_size)
+{
+	int lo = 0;
+	int hi = arr_size - 1;
+	int mid = 0;
+
+	while (mid <= hi) {
+		switch (a[mid]) {
+		case 0:
+			swap(a[lo++], a[mid++]);
+			break;
+
+		case 1:
+			mid++;
+			break;
+
+		case 2:
+			swap(a[mid], a[hi--]);
+			break;
+		}
+	}
+}
+
+void printArray(int arr[], int arr_size)
+{
+	for (int i = 0; i < arr_size; i++)
+		cout << arr[i] << " ";
+}
+
+int main()
+{
+	int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	sort012(arr, n);
+	printArray(arr, n);
+	return 0;
+}
