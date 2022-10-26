a5e76dc1a1b8e7a316676ecf11586e4463f7540f
itsrahulprince <91690860+itsrahulprince@users.noreply.github.com>
2022-10-26 21:32:05 +0530

    Update GroupAnagrams.cpp

--- C++/GroupAnagrams.cpp
+++ C++/GroupAnagrams.cpp
@@ -1,13 +1,54 @@
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>mp;
-        for(int i=0; i<strs.size(); i++){
-            string s=strs[i];
-            sort(s.begin(),s.end());
-            mp[s].push_back(strs[i]);
-        }
-        vector<vector<string>>ans;
-        for(auto it:mp){
-            ans.push_back(it.second);
-        }
-        return ans;
-    }
+#include <bits/stdc++.h>
+using namespace std;
+
+void solver(vector<string> my_list)
+{
+	map<map<char, int>, vector<string>> my_map;
+
+	for(string str : my_list)
+	{
+		map<char, int> temp_map;
+		vector<string> temp_my_list;
+		for(int i = 0; i < str.length(); ++i)
+		{
+			++temp_map[str[i]];
+		}
+		
+		auto it = my_map.find(temp_map);
+		if (it != my_map.end())
+		{
+			it->second.push_back(str);
+		}
+		else
+		{
+			temp_my_list.push_back(str);
+			my_map.insert({ temp_map, temp_my_list });
+		}
+	}
+
+	vector<vector<string>> result;
+
+	for(auto it = my_map.begin();
+			it != my_map.end(); ++it)
+	{
+		result.push_back(it->second);
+	}
+
+	for(int i = 0; i < result.size(); ++i)
+	{
+		cout << "[";
+		for(int j = 0; j < result[i].size(); ++j)
+		{
+			cout << result[i][j] << ", ";
+		}
+		cout << "]";
+	}
+}
+
+int main()
+{
+	vector<string> my_list = { "cat", "dog", "ogd",
+							"god", "atc" };
+	solver(my_list);
+	return 0;
+}
